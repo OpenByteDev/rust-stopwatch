a21724c3061e5279e70f986686d3d4ccd020b8db
jojolepro <jojolepro@jojolepro.com>
2021-04-30 12:47:57 -0400

    simplify crate and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,17 @@ use std::default::Default;
 use std::fmt;
 use std::time::{Duration, Instant};
 
-// default zero
-// can start
-// can check time
-// can split
-// can temporarily see split time
-// can check time since start
-// can check all splits times
-// can stop
-// can check elapsed time until stop
-// can check all splits
-//
-// [start]...[split1]...[split2]...[pause]   [start]...[split3]
-// 
-// total / elapsed since start
-// [                                     ] + [                ]
-
+/// A span of time that is started but might not have an end yet.
 #[derive(Clone, Debug)]
-pub struct TimeSplit {
-    start: Instant,
-    stop: Option<Instant>,
+pub struct TimeSpan {
+    /// The instant at which the span started.
+    pub start: Instant,
+    /// The instant at which the span stopped, if any.
+    pub stop: Option<Instant>,
 }
 
-impl Into<Duration> for TimeSplit {
+/// Converts a TimeSpan into a Duration.
+impl Into<Duration> for TimeSpan {
     fn into(self) -> Duration {
         if let Some(stop) = self.stop {
             stop - self.start
@@ -34,17 +22,31 @@ impl Into<Duration> for TimeSplit {
     }
 }
 
-// if your last time split doesn't have a stop time, you are still running
+// if your last time span doesn't have a stop time, you are still running
 
 /// A stopwatch used to calculate time differences.
+/// # Example
+/// ```rust
+/// use stopwatch2::*;
+/// let mut s = Stopwatch::default();
+/// s.start(); // Starts the stopwatch.
+/// s.start(); // Creates a new span.
+/// s.stop(); // Stops the stopwatch.
+/// println!("{}", s); // Prints the total time.
+/// println!("{:?}", s); // Prints the different time spans.
+/// s.spans.clear(); // Reset the stopwatch.
+/// println!("{}", s); // Prints the total time.
+/// println!("{:?}", s); // Prints the different time spans.
+/// ```
 #[derive(Clone, Default, Debug)]
 pub struct Stopwatch {
-    /// The time the stopwatch was split last, if ever.
-    splits: Vec<TimeSplit>,
-    /// The time elapsed while the stopwatch was running (between start() and stop()).
-    elapsed: Duration,
+    /// All time spans that this stopwatch has run or is running.
+    /// Only the last timespan is allowed to have no stop value, which means it
+    /// is still active.
+    pub spans: Vec<TimeSpan>,
 }
 
+/// Prints the total time this Stopwatch has run.
 impl fmt::Display for Stopwatch {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         return write!(f, "{}s", self.elapsed().as_secs_f64());
@@ -52,72 +54,165 @@ impl fmt::Display for Stopwatch {
 }
 
 impl Stopwatch {
-    /// Creates a new `Stopwatch`.
-    pub fn new() -> Stopwatch {
-        Default::default()
-    }
-
     /// Starts the stopwatch.
-    pub fn start(&mut self) {
-        self.start_time = Some(Instant::now());
+    ///
+    /// If it is already started, it will create a new split.
+    /// This means it will stop and start the stopwatch, creating a new TimeSpan
+    /// in the process.
+    pub fn start(&mut self) -> Option<TimeSpan> {
+        // if no split or last split is stopped, create new one.
+        let ret = self.stop();
+        self.spans.push(TimeSpan {
+            start: Instant::now(),
+            stop: None,
+        });
+        return ret;
     }
 
     /// Stops the stopwatch without resetting it.
-    pub fn stop(&mut self) -> Duration {
-        self.elapsed = self.elapsed();
-        self.start_time = None;
-        self.splits.clear();
-        self.elapsed
-    }
-
-    /// Resets all counters and stops the stopwatch.
-    pub fn reset(&mut self) {
-        self.elapsed = Duration::from_secs(0);
-        self.start_time = None;
-        self.splits.clear();
-    }
-
-    /// Resets and starts the stopwatch again.
-    pub fn restart(&mut self) {
-        self.reset();
-        self.start();
+    pub fn stop(&mut self) -> Option<TimeSpan> {
+        let mut ret = None;
+        if self.is_running() {
+            self.spans.last_mut().unwrap().stop = Some(Instant::now());
+            ret = Some(self.spans.last().unwrap().clone());
+        }
+        return ret;
     }
 
     /// Returns whether the stopwatch is running.
     pub fn is_running(&self) -> bool {
-        return self.start_time.is_some();
+        // if no spans or last span has an end, we are not running.
+        // equiv: if we have splits and the last one has no stop
+        !self.spans.is_empty() && self.spans.last().unwrap().stop.is_none()
     }
 
-    /// Returns the elapsed time since the start of the stopwatch.
+    /// Returns the total elapsed time accumulated inside of this stopwatch.
     pub fn elapsed(&self) -> Duration {
-        match self.start_time {
-            // stopwatch is running
-            Some(t1) => {
-                return t1.elapsed() + self.elapsed;
-            }
-            // stopwatch is not running
-            None => {
-                return self.elapsed;
-            }
-        }
-    }
-
-    /// Returns the elapsed time since last split or start/restart.
-    ///
-    /// If the stopwatch is in stopped state this will always return a zero Duration.
-    pub fn split(&mut self) -> Duration {
-        match self.start_time {
-            // stopwatch is running
-            Some(start) => {
-                let res = match self.splits.last() {
-                    Some(split) => split.elapsed(),
-                    None => start.elapsed(),
-                };
-                self.splits.push(Instant::now());
-                res
-            }
-            // stopwatch is not running
-            None => Duration::from_secs(0),
-        }
+        // better way to do the conversion here?
+        self.spans.iter().map(|s| {let d: Duration = s.clone().into(); d}).sum()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use crate::*;
+
+    static SLEEP_MS: u64 = 50;
+    static TOLERANCE_PERCENTAGE: f64 = 0.3;
+
+    #[test]
+    fn repeated_stops() {
+        let mut sw = Stopwatch::default();
+        for _ in 0..1000 {
+            sw.start();
+        }
+        sw.stop();
+        assert_eq!(sw.spans.len(), 1000);
+        assert!(sw.spans.last().unwrap().stop.is_some());
+    }
+    
+    #[test]
+    fn elapsed_none() {
+        let mut sw = Stopwatch::default();
+        sw.stop();
+        sw.stop();
+        assert_eq!(sw.elapsed().as_secs_f32(), 0.0);
+    }
+    
+    #[test]
+    fn elapsed_ms() {
+        let mut sw = Stopwatch::default();
+        sw.start();
+        sleep_ms(SLEEP_MS);
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+    }
+    
+    #[test]
+    fn stop() {
+        let mut sw = Stopwatch::default();
+        sw.start();
+        sleep_ms(SLEEP_MS);
+        sw.stop();
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+        sleep_ms(SLEEP_MS);
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+    }
+    
+    #[test]
+    fn resume_once() {
+        let mut sw = Stopwatch::default();
+        assert_eq!(sw.spans.len(), 0);
+        sw.start();
+        assert_eq!(sw.spans.len(), 1);
+        sleep_ms(SLEEP_MS);
+        sw.stop();
+        assert_eq!(sw.spans.len(), 1);
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+        sw.start();
+        assert_eq!(sw.spans.len(), 2);
+        sleep_ms(SLEEP_MS);
+        assert_duration_near(sw.elapsed(), 2 * SLEEP_MS);
+    }
+    
+    #[test]
+    fn resume_twice() {
+        let mut sw = Stopwatch::default();
+        assert_eq!(sw.spans.len(), 0);
+        sw.start();
+        sleep_ms(SLEEP_MS);
+        sw.stop();
+        assert_eq!(sw.spans.len(), 1);
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+        sw.start();
+        assert_eq!(sw.spans.len(), 2);
+        sleep_ms(SLEEP_MS);
+        sw.start();
+        assert_eq!(sw.spans.len(), 3);
+        assert_duration_near(sw.elapsed(), 2 * SLEEP_MS);
+        sw.start();
+        assert_eq!(sw.spans.len(), 4);
+        sleep_ms(SLEEP_MS);
+        assert_duration_near(sw.elapsed(), 3 * SLEEP_MS);
+    }
+    
+    #[test]
+    fn is_running() {
+        let mut sw = Stopwatch::default();
+        assert!(!sw.is_running());
+        sw.start();
+        assert!(sw.is_running());
+        sw.stop();
+        assert!(!sw.is_running());
+    }
+    
+    #[test]
+    fn reset() {
+        let mut sw = Stopwatch::default();
+        sw.start();
+        sleep_ms(SLEEP_MS);
+        sw.spans.clear();
+        assert!(!sw.is_running());
+        sw.start();
+        sleep_ms(SLEEP_MS);
+        assert_duration_near(sw.elapsed(), SLEEP_MS);
+    }
+    
+    // helpers
+    fn sleep_ms(ms: u64) {
+        std::thread::sleep(Duration::from_millis(ms))
+    }
+    
+    fn assert_near(x: i64, y: i64, tolerance: u64) {
+        let diff = (x - y).abs() as u64;
+        if diff > tolerance {
+            panic!("Expected {:?}, got {:?}", x, y);
+        }
+    }
+    
+    fn assert_duration_near(duration: Duration, elapsed: u64) {
+        let tolerance_value = (TOLERANCE_PERCENTAGE * elapsed as f64) as u64;
+        assert_near(elapsed as i64, duration.as_millis() as i64, tolerance_value);
+    }
+}
+
